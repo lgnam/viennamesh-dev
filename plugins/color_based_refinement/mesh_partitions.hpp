efb53102c905e27f77b1234f7d10477991c86c01
Lukas Gnam <gnam@iue.tuwien.ac.at>
2017-03-29 12:13:23 +0200

    started implementing color-based-refinement

---
+++ plugins/color_based_refinement/mesh_partitions.hpp
@@ -0,0 +1,49 @@
+#ifndef MESH_PARTITIONS_HPP
+#define MESH_PARTITIONS_HPP
+
+//pragmatic basic includes for data structure
+//TODO: implement own data structure
+
+//all other includes
+
+//----------------------------------------------------------------------------------------------------------------------------------------------//
+//                                                                Declaration                                                                   //
+//----------------------------------------------------------------------------------------------------------------------------------------------//
+
+//class MeshPartitions
+//
+//This class partitions an input mesh and stores its partitions in different data structurs for further processing
+class MeshPartitions
+{
+    public:
+        MeshPartitions(Mesh<double>* original_mesh, int num_regions);          //Constructor
+        ~MeshPartitions();
+                                                                          //Destructor
+    private:
+}; //end of class MeshPartitions
+
+//----------------------------------------------------------------------------------------------------------------------------------------------//
+//                                                                Helper Functions                                                              //
+//----------------------------------------------------------------------------------------------------------------------------------------------//
+
+//----------------------------------------------------------------------------------------------------------------------------------------------//
+//                                                                Implementation                                                                //
+//----------------------------------------------------------------------------------------------------------------------------------------------//
+
+//Constructor
+//
+//Tasks: TODO
+MeshPartitions::MeshPartitions(Mesh<double>* original_mesh, int num_regions)
+{
+    std::cout << "Calling Constructor of MeshPartitions" << std::endl;
+} //end of Constructor
+
+//Destructor
+//
+//Tasks: TODO
+MeshPartitions::~MeshPartitions()
+{
+    std::cout << "Calling Destructor of MeshPartitions" << std::endl;
+} //end of Destructor
+
+#endif
---
+++ plugins/color_based_refinement/plugin.cpp
@@ -0,0 +1,11 @@
+#include "viennameshpp/plugin.hpp"
+
+viennamesh_error viennamesh_plugin_init(viennamesh_context context)
+{  
+  return VIENNAMESH_SUCCESS;
+}
+
+int viennamesh_version()
+{
+  return VIENNAMESH_VERSION;
+}
